b7cacbc7fe21d759515368de94a8dcfc8d319b42
Aleks Stefanov <AStefanov@corp.globetax.com>
2019-06-13 22:03:59 -0400

    publicPath revert

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/trp' : '/',
+  publicPath: '/trp',
   configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
