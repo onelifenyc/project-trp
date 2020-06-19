cddaf43d93b72845e07eb72e517d6b512246d6c1
Aleks Stefanov <AStefanov@corp.globetax.com>
2020-06-19 01:39:03 -0400

    outputDir > /docs

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/trp/' : '/',
+  outputDir: 'docs',
   configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
